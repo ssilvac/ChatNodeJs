41a79d926bf2ffe276ab06646910195e0cd68a9b
ssilvac <sebastian.silva@hotmail.es>
2015-10-30 12:00:22 -0300

    se identifican los usuarios

--- app.js
+++ app.js
@@ -1,7 +1,8 @@
 var express = require("express"),
 	app = express(),
 	server = require("http").createServer(app),
-	io = require("socket.io").listen(server);
+	io = require("socket.io").listen(server),
+	nicknames = {};
 
 
 //server.listen(process.env.PORT, process.env.IP);
@@ -13,12 +14,45 @@ app.get("/", function(req, res){
 
 });
 
+
+// SOCKETS
 // cada vez que alguien se conecte se crea un nuevo socket
-io.sockets.on('connection', function(socket){
+io.sockets.on( ('connection'), function(socket){
 	
 	socket.on('sendMessage', function(data){
 
-		io.sockets.emit('newMessage', {msg: data});
+		io.sockets.emit('newMessage', {msg: data, nick: socket.nickname});
 
 	});
+
+	socket.on('newUser', function(data, callback){
+
+		if(data in nicknames){
+			callback(false);
+		}else{
+
+			callback(true);
+			socket.nickname = data;
+			nicknames[socket.nickname] = 1;
+
+			updateNickNames();
+			
+		}
+
+	});
+
+	socket.on('disconnect', function(data){
+
+		if(!socket.nickname) return false;
+
+		delete nicknames[socket.nickname];
+
+		updateNickNames();
+
+	});
+
+	function updateNickNames()
+	{
+		io.sockets.emit('usernames', nicknames);
+	}
 });
